d961fba8ce0417aac7b5c92ce9c356c5e2f363ca
haydenfelfe <felfehayden@gmail.com>
2023-06-28 19:27:10 -0700

    Removed unnecessary sql files from employee-tracker

--- add_departments.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO department (name) VALUES ('Sales');
-INSERT INTO department (name) VALUES ('Marketing');
-INSERT INTO department (name) VALUES ('Finance');
--- add_roles.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO role (title, salary, department_id)
-VALUES ('Sales Manager', 50000, 10),
-       ('Marketing Coordinator', 40000, 11),
-       ('Finance Analyst', 45000, 12);
